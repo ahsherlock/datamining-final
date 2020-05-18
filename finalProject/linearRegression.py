cdfad7c4dcee75287e7c18be0edd2778ed079833
Alec Sherlock <38014153+ahsherlock@users.noreply.github.com>
2020-05-17 22:16:18 -0700

    Add files via upload

--- finalProject/linearRegression.py
+++ finalProject/linearRegression.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 plt.rcParams['figure.figsize'] = (10.0, 7.0)
+plt.style.use('ggplot')
 # found the data set at https://www.kaggle.com/sidtwr/videogames-sales-dataset?select=Video_Games_Sales_as_at_22_Dec_2016.csv
 
 def splitThatShit(dataframe):
@@ -21,6 +22,19 @@ def showDataCorrelation(columnName, dataFrame):
     plt.scatter(X, Y)
     plt.show()
 
+def trainLeastSquare(trainX, trainY):
+    meanX = np.mean(trainX)
+    meanY = np.mean(trainY)
+    num = 0
+    den = 0
+    for i in range(len(trainX)):
+        num += (trainX[i] - meanX) * (trainY[i] - meanY)
+        den += (trainX[i] - meanX)**2
+    coEffM = num/den
+    coEffC = meanY - m*meanX
+    return coEffM, coEffC
+
+
 def trainGradientDescent(trainX, trainY,learningRate, iterations):
     m = 0
     c = 0
@@ -36,18 +50,50 @@ def trainGradientDescent(trainX, trainY,learningRate, iterations):
         c = c - L * D_c  # Update c
         return m, c
 
+def predictYValues(testingX,predictedY,m,c):
+    for i in range(len(testingX)):
+        predictedY.append(m * testingX[i] + c)
+    return predictedY
+
+def getRSquare(X,Y,m, c):
+    ss_t = 0
+    ss_r = 0
+    for i in range(len(X)):
+        yPrediction = c + m*X[i]
+        ss_t += (Y[i] - np.mean(Y))**2
+        ss_r += (Y[i] - yPrediction)**2
+    rSquare = 1 - (ss_r/ss_t)
+    return rSquare
+
+def getRMSE(X,Y, m , c):
+    mse = 0
+    for i in range(len(X)):
+        yPrediction = c + m*X[i]
+        mse += (Y[i] - yPrediction)**2
+    rmse = np.sqrt(mse/len(X))
+    return rmse
+
+def minMaxNormalization(dataFrame):
+    dataFrame = (dataFrame - dataFrame.min())/(dataFrame.max() - dataFrame.min())
+    return dataFrame
+def makeNumeric(dataFrame):
+    dataFrame = dataFrame.astype('float')
+    return dataFrame
+
 
 # PREPROCESSING OF THE DATA
 
-data = pd.read_csv('vgsales2019short.csv', usecols=['Global_Sales', 'User_Score', 'Critic_Score'
-    , 'Year'])
-dataClean = data.dropna(subset=['User_Score', 'Critic_Score', 'Year', 'Global_Sales'])
+data = pd.read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv', usecols=['Global_Sales', 'User_Score', 'Critic_Score'])
+dataClean = data.dropna(subset=['User_Score', 'Critic_Score','Global_Sales'])
 dataClean = dataClean[dataClean['User_Score'] != 'tbd']
+dataClean = makeNumeric(dataClean)
 dataClean['User_Score_Numeric'] = pd.to_numeric(dataClean['User_Score'])
 dataClean['Critic_Score_Numeric'] = pd.to_numeric(dataClean['Critic_Score'])
 dataClean['User_Score_Numeric'] = dataClean['User_Score_Numeric'].apply(lambda x: 10 * x)
+
 print("ORIGINAL DATA BEFORE THE SPLIT")
-print(dataClean.head())
+print(dataClean.info())
+print(dataClean.head(5))
 print(dataClean.describe())
 print("\n**************************************************")
 print("\n")
@@ -56,9 +102,10 @@ print("\n")
 
 training, testing = splitThatShit(dataClean)
 print("Training Data")
+print(training.info())
 print(training.describe())
 print(training.head(25))
-print(training['Global_Sales'])
+#print(training['Global_Sales'])
 print("\n")
 print("Testing Data")
 print(testing.describe())
@@ -69,84 +116,111 @@ print(dataClean.describe())
 print(dataClean.head(25))
 
 # Show data correlations
-showDataCorrelation("User_Score_Numeric", dataClean)
-showDataCorrelation("Critic_Score_Numeric", dataClean)
-# Y = dataClean.iloc[:, 1]
-# X = dataClean.iloc[:, 2]
-# plt.xlabel("User Score")
-# plt.ylabel("Global Sales")
-# plt.scatter(X, Y)
-# plt.show()
-#
-# X = dataClean.iloc[:, 4]
-# plt.xlabel("Critic Score")
-# plt.ylabel("Global Sales")
-# plt.scatter(X, Y)
-# plt.show()
-#
-# X = dataClean.iloc[:, 0]
-# plt.xlabel("Year of Release")
-# plt.ylabel("Global Sales")
-# plt.scatter(X, Y)
-# plt.show()
+#showDataCorrelation("User_Score_Numeric", dataClean)
+#showDataCorrelation("Critic_Score_Numeric", dataClean)
 
-# X = data.iloc[:, 0]
-# Y = data.iloc[:, 1]
-# plt.scatter(X, Y)
-# plt.show()
-#
 
-print("BUILDING MODEL")
+print("********** BUILDING MODEL USING GRADIENT DESCENT FOR USER SCORE *************")
 # Building the model for Gradient Descent
-X = training.iloc[:, 4]
-print(X.describe())
-Y = training.iloc[:, 1]
-print(Y.describe())
+X = training["User_Score_Numeric"].values
+Y = training["Global_Sales"].values
+Y = minMaxNormalization(Y)
 m, c = trainGradientDescent(X, Y, 0.0001, 1000)
-testingX = testing.iloc[:, 4]
+testingX = testing["User_Score_Numeric"].values
+predictedY = []
+predictedY = predictYValues(testingX,predictedY,m,c)
+plt.plot(testingX, predictedY, c="#52b920", label="Regression Line")
+plt.scatter(X, Y, c="#ef4423", label = "Scatter plot")
+plt.title("Global Sales v User Score (Gradient Descent)")
+plt.xlabel('USER SCORE')
+plt.ylabel('GLOBAL SALES')
+plt.legend()
+plt.show()
+print("CoEff1 = "+str(m))
+print("CoEff2 = "+str(c))
+#R Square
+rSquareGradDecUser = getRSquare(X,Y,m,c)
+print("R2 Value for gradient descent on User Score = " + str(rSquareGradDecUser))
+#Root Mean Square Error
+rootMeanSquareError = getRMSE(X,Y,m,c)
+#mse = np.sum((predictedY - Y)**2)
+#rootMeanSquareError = np.sqrt(mse/len(X))
+print("The Root Mean Square Error for gradient descent on User Score = " + str(rootMeanSquareError)+ "\n\n")
 
-# Performing Gradient Descent
-# for i in range(epochs):
-#     Y_pred = m * X + c  # The current predicted value of Y
-#     D_m = (-2 / n) * sum(X * (Y - Y_pred))  # Derivative wrt m
-#     D_c = (-2 / n) * sum(Y - Y_pred)  # Derivative wrt c
-#     m = m - L * D_m  # Update m
-#     c = c - L * D_c  # Update c
+
+print("********** BUILDING MODEL USING GRADIENT DESCENT FOR CRITIC SCORE *************")
+# Building the model for Gradient Descent
+X = training["Critic_Score_Numeric"].values
+Y = training["Global_Sales"].values
+Y = minMaxNormalization(Y)
+m, c = trainGradientDescent(X, Y, 0.0001, 1000)
+testingX = testing["Critic_Score_Numeric"].values
+#Predict values
+predictedY = m*testingX+c
+plt.plot(testingX, predictedY, c="#52b920", label="Regression Line")
+plt.scatter(X, Y, c="#ef4423", label="Scatter plot")
+plt.title('GLOBAL SALES v. CRITIC SCORE(Gradient Descent)')
+plt.xlabel('CRITIC SCORE')
+plt.ylabel('GLOBAL SALES')
+plt.legend()
+plt.show()
+print("CoEff1(m) = "+str(m))
+print("CoEff2(c) = "+str(c))
+#R Square
+rSquareGradDecUser = getRSquare(X, Y, m, c)
+print("R2 Value for gradient descent on Critic Score = " + str(rSquareGradDecUser))
+#Root Mean Square Error
+rootMeanSquareError = getRMSE(X, Y, m, c)
+print("The Root Mean Square Error for gradient descent on Critic Score = " + str(rootMeanSquareError) + "\n\n")
 
 
-print(m, c)
-# Making predictions
-Y_pred = m*testingX + c
 
-plt.scatter(X, Y)
-plt.xlabel("User Score")
-plt.ylabel("Global Sales")
-plt.title("Gradient Descent Linear Regression")
-plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
+
+print("********** BUILDING MODEL USING LEAST SQUARE FOR USER SCORE *************")
+X = training['User_Score_Numeric'].values
+Y = training['Global_Sales'].values
+Y = minMaxNormalization(Y)
+m, c = trainLeastSquare(X,Y)
+testingX = testing['User_Score_Numeric'].values
+Y_pred = m*X + c
+print("CoEff1(m): " + str(m))
+print("CoEff2(c): " + str(c))
+rSquareLeastUser = getRSquare(X, Y, m, c)
+print("R Square for Least Square Method on Global Sales v User Score: " + str(rSquareLeastUser))
+rootMeanSquareError = getRMSE(X, Y, m, c)
+print("The Root Mean Square Error: " + str(rootMeanSquareError))
+print("\n\n")
+plt.title("Global Sales v User Score (Least Square)")
+plt.xlabel('Critic SCORE')
+plt.ylabel('GLOBAL SALES')
+plt.legend()
+plt.scatter(X, Y, color='purple', label=('Scatter Plot'))
+#predicted graph
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='green', label='Regression Line')
 plt.show()
 
 
-# Build Model for Least Squares
 
-# X_mean = np.mean(X)
-# Y_mean = np.mean(Y)
-#
-# num = 0
-# den = 0
-# for i in range(len(X)):
-#     num += (X[i] - X_mean)*(Y[i] - Y_mean)
-#     den += (X[i] - X_mean)**2
-# m = num / den
-# c = Y_mean - m*X_mean
-#
-# print (m, c)
+print("********** BUILDING MODEL USING LEAST SQUARE FOR CRITIC SCORE *************")
+X = training['Critic_Score_Numeric'].values
+Y = training['Global_Sales'].values
+Y = minMaxNormalization(Y)
+m, c = trainLeastSquare(X,Y)
+testingX = testing['Critic_Score_Numeric'].values
+Y_pred = m*testingX + c
+print("CoEff1(m): " + str(m))
+print("CoEff2(c): " + str(c))
+rSquareLeastUser = getRSquare(X, Y, m, c)
+print("R Square for Least Square Method on Global Sales v Critic Score: " + str(rSquareLeastUser))
+rootMeanSquareError = getRMSE(X, Y, m, c)
+print("The Root Mean Square Error: " + str(rootMeanSquareError))
+print("\n\n")
+plt.title("Global Sales v Critic Score (Least Square)")
+plt.xlabel('USER SCORE')
+plt.ylabel('GLOBAL SALES')
+plt.legend()
+plt.scatter(X, Y, color ='purple', label="Scatter Plot")
+#predicted graph
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='green', label="Regression Line")
+plt.show()
 
-
-# Making predictions Least Squares
-# Y_pred = m*X + c
-# plt.xlabel("raddaradda")
-# plt.ylabel("raddagradgsdfs")
-# plt.title("Least Squares Linear Regression")
-# plt.scatter(X, Y) # actual
-# plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red') # predicted
-# plt.show()
